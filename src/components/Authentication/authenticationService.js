b0b6764c62462004ba326aebcedd7880d3dffde4
trihims <himanshu.16bcs1156@abes.ac.in>
2023-03-06 14:09:35 +0530

    stripe integration

--- src/components/Authentication/authenticationService.js
+++ src/components/Authentication/authenticationService.js
@@ -1,218 +1,205 @@
-import { firebaseLogout as firebaseLogout, initializeFirebase, firebaseSignInWithFacebook, firebaseSignInUserEmailNPass, firebaseCreateUserEmailNPass, firebaseSignInWithGoogle, firebaseSendPasswordResetLink, firebaseGetConfirmEmailLink, firebaseAfterConfirmApplyCode, firebaseChangePassword } from './auth_firebase.js'
+import {
+  firebaseLogout as firebaseLogout,
+  initializeFirebase,
+  firebaseSignInWithFacebook,
+  firebaseSignInUserEmailNPass,
+  firebaseCreateUserEmailNPass,
+  firebaseSignInWithGoogle,
+  firebaseSendPasswordResetLink,
+  firebaseGetConfirmEmailLink,
+  firebaseAfterConfirmApplyCode,
+  firebaseChangePassword,
+} from "./auth_firebase.js";
 
-import store from '../store.js'
+import store from "../store.js";
 
-import { authSlice } from './AuthSlice'
+import { authSlice } from "./AuthSlice";
 
+var __authHandle;
 
+const { loading, unauthorized, authorized, emailNotConfirm } =
+  authSlice.actions;
 
-var __authHandle
+const confirmLoginPromise = async (callback = () => {}) => {
+  let cancel;
+  let prom = new Promise(
+    (resolve, reject) =>
+      (cancel = __authHandle.onAuthStateChanged(
+        (user) => {
+          if (user?.email) {
+            resolve(user);
+          } else {
+            resolve({});
+          }
+        },
+        (err) => {
+          reject(err);
+        }
+      ))
+  );
 
+  let result = await prom;
 
-const { loading, unauthorized, authorized, emailNotConfirm } = authSlice.actions
+  callback(result);
 
-const confirmLoginPromise = async (callback = () => { }) => {
-    let cancel;
-    let prom = new Promise((resolve, reject) =>
-        cancel = __authHandle.onAuthStateChanged(user => {
-            if (user?.email) {
-                resolve(user)
-            } else {
-                resolve({})
-            }
-        }, err => {
-            reject(err)
-        })
-    )
+  cancel();
 
-    let result = await prom
+  return result;
+};
 
-    callback(result)
+const confirmEmailPromise = async (success = () => {}, failure = () => {}) => {
+  let cancel;
+  let timeOutHandle;
+  let prom = new Promise((resolve, reject) => {
+    cancel = __authHandle.onAuthStateChanged(
+      (user) => {
+        if (user?.emailVerified) {
+          resolve(user);
+        }
+      },
+      (err) => {
+        reject(err);
+      }
+    );
 
-    cancel();
+    timeOutHandle = setTimeout(() => {
+      !!reject && reject("Time out");
+    }, 600000);
+  });
 
-
-    return result
-}
-
-
-const confirmEmailPromise = async (success = () => { }, failure = () => { }) => {
-    let cancel;
-    let timeOutHandle;
-    let prom = new Promise((resolve, reject) => {
-        cancel = __authHandle.onAuthStateChanged(user => {
-            if (user?.emailVerified) {
-                resolve(user)
-            }
-        }, err => {
-            reject(err)
-        });
-
-        timeOutHandle = setTimeout(() => {
-            !!reject && reject("Time out")
-        }, 600000)
-    }
-    )
-
-    try {
-        let result = await prom
-        success(user)
-
-    } catch (error) {
-        failure()
-    } finally {
-        cancel && cancel();
-        timeOutHandle && timeOutHandle();
-
-    }
-
-}
-
+  try {
+    let result = await prom;
+    success(user);
+  } catch (error) {
+    failure();
+  } finally {
+    cancel && cancel();
+    timeOutHandle && timeOutHandle();
+  }
+};
 
 const userStateDispachFlow = (user) => {
+  let authUser = user;
+  console.log("printuser");
 
-    let authUser = user
-    console.log("printuser")
-
-    if (authUser.providerData[0].providerId === "password"
-        && authUser.emailVerified === false) {
-        console.log("reached to Email not verified")
-        store.dispatch(emailNotConfirm(JSON.stringify(user)))
-
-
-    } else {
-        store.dispatch(authorized(JSON.stringify(user)))
-    }
-}
+  if (
+    authUser.providerData[0].providerId === "password" &&
+    authUser.emailVerified === false
+  ) {
+    console.log("reached to Email not verified");
+    store.dispatch(emailNotConfirm(JSON.stringify(user)));
+  } else {
+    store.dispatch(authorized(JSON.stringify(user)));
+  }
+};
 
 export const initializeAuthentication = async () => {
+  store.dispatch(loading());
+  let auth = initializeFirebase();
 
-    store.dispatch(loading())
-    let auth = initializeFirebase();
+  __authHandle = auth;
 
-    __authHandle = auth
-
-
-
-    let user = await confirmLoginPromise()
-    if (user?.email) {
-        userStateDispachFlow(user)
-    }
-    else {
-        store.dispatch(unauthorized())
-    }
-}
-
+  let user = await confirmLoginPromise();
+  if (user?.email) {
+    userStateDispachFlow(user);
+  } else {
+    store.dispatch(unauthorized());
+  }
+};
 
 export const signInWithFaceBook = async () => {
-    let auth = __authHandle;
-    store.dispatch(loading())
-    let loginObj = await firebaseSignInWithFacebook(auth)
+  let auth = __authHandle;
+  store.dispatch(loading());
+  let loginObj = await firebaseSignInWithFacebook(auth);
 
-
-    if (loginObj?.user?.email) {
-        store.dispatch(authorized(JSON.stringify(loginObj.user)))
-    } else {
-        store.dispatch(unauthorized())
-    }
-}
+  if (loginObj?.user?.email) {
+    store.dispatch(authorized(JSON.stringify(loginObj.user)));
+  } else {
+    store.dispatch(unauthorized());
+  }
+};
 export const signInWithGoogle = async () => {
-    let auth = __authHandle;
-    store.dispatch(loading())
-    let loginObj = await firebaseSignInWithGoogle(auth)
+  let auth = __authHandle;
+  store.dispatch(loading());
+  let loginObj = await firebaseSignInWithGoogle(auth);
 
-
-    if (loginObj?.user?.email) {
-        store.dispatch(authorized(JSON.stringify(loginObj.user)))
-    } else {
-        store.dispatch(unauthorized())
-    }
-}
-
+  if (loginObj?.user?.email) {
+    store.dispatch(authorized(JSON.stringify(loginObj.user)));
+  } else {
+    store.dispatch(unauthorized());
+  }
+};
 
 export const logout = async (callback) => {
-    let auth = __authHandle
-    store.dispatch(loading())
-    firebaseLogout(auth)
-    store.dispatch(unauthorized())
-    !!callback && callback()
-}
-
+  let auth = __authHandle;
+  store.dispatch(loading());
+  firebaseLogout(auth);
+  store.dispatch(unauthorized());
+  !!callback && callback();
+};
 
 export const waitUntilEmailIsConfrimed = async () => {
-    await sendConfirmEmail()
+  await sendConfirmEmail();
 
-    confirmEmailPromise((user) => {
-        store.dispatch(authorized(JSON.stringify(user)))
+  confirmEmailPromise(
+    (user) => {
+      store.dispatch(authorized(JSON.stringify(user)));
     },
-        () => {
-            logout()
-        }
-    )
-}
-
+    () => {
+      logout();
+    }
+  );
+};
 
 export const sendConfirmEmail = async () => {
-
-    let auth = __authHandle
-    await firebaseGetConfirmEmailLink(auth)
-
-
-
-}
-
-
+  let auth = __authHandle;
+  await firebaseGetConfirmEmailLink(auth);
+};
 
 export const signInUser = async (email, password) => {
-    let auth = __authHandle
-    store.dispatch(loading())
-    let loginObj = await firebaseSignInUserEmailNPass(auth, email, password)
-    if (loginObj?.user?.email) {
-        userStateDispachFlow(loginObj.user)
-    } else {
-        store.dispatch(unauthorized())
-    }
-
-}
+  let auth = __authHandle;
+  store.dispatch(loading());
+  let loginObj = await firebaseSignInUserEmailNPass(auth, email, password);
+  if (loginObj?.user?.email) {
+    userStateDispachFlow(loginObj.user);
+  } else {
+    store.dispatch(unauthorized());
+  }
+};
 
 export const createInUser = async (email, password) => {
-    let auth = __authHandle
-    store.dispatch(loading())
-    let loginObj = await firebaseCreateUserEmailNPass(auth, email, password)
-    if (loginObj?.user?.email) {
-        store.dispatch(authorized(JSON.stringify(loginObj.user)))
-    } else {
-        store.dispatch(unauthorized())
-    }
+  let auth = __authHandle;
+  store.dispatch(loading());
+  let loginObj = await firebaseCreateUserEmailNPass(auth, email, password);
+  if (loginObj?.user?.email) {
+    store.dispatch(authorized(JSON.stringify(loginObj.user)));
+  } else {
+    store.dispatch(unauthorized());
+  }
+};
 
-}
-
-export const sendPasswordResetLink = async (email, beforeFn = () => { }, afterFn = () => { }) => {
-    beforeFn()
-    let auth = __authHandle
-    await firebaseSendPasswordResetLink(auth, email);
-    afterFn();
-}
-
+export const sendPasswordResetLink = async (
+  email,
+  beforeFn = () => {},
+  afterFn = () => {}
+) => {
+  beforeFn();
+  let auth = __authHandle;
+  await firebaseSendPasswordResetLink(auth, email);
+  afterFn();
+};
 
 export const changePasswordService = async (password) => {
-    let auth = __authHandle
-    let res = await firebaseChangePassword(auth.currentUser,password)
-
-}
+  let auth = __authHandle;
+  let res = await firebaseChangePassword(auth.currentUser, password);
+};
 
 store.subscribe(() => {
-    console.info(JSON.stringify(store.getState().authentication))
-})
-
-
+  console.info(JSON.stringify(store.getState().authentication));
+});
 
 export const isUserAuthenticated = () => {
-    let currentState = store.getState().authentication
-    let authUser = JSON.parse(currentState.authUser);
-    return authUser && authUser.email && !currentState.confirmEmail;
-}
-
-
-
-
+  let currentState = store.getState().authentication;
+  let authUser = JSON.parse(currentState.authUser);
+  return authUser && authUser.email && !currentState.confirmEmail;
+};
--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MiddleWare from './MiddleWare';
 import store from '../components/store';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Provider } from 'react-redux'
 
